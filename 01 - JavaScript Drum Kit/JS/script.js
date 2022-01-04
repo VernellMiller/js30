3e09ef58f1a67816590f376c10ce751e9ceeb0e4
vernellmiller <nezeey@gmail.com>
2022-01-04 12:48:05 -0500

    adds comments and step 2

--- 01 - JavaScript Drum Kit/JS/script.js
+++ 01 - JavaScript Drum Kit/JS/script.js
@@ -1,3 +1,60 @@
+/* WINDOW - represents the browser's window.
+    - all global JavaScript objects, functions and variables automatically      become part of the WINDOW object.
+
+    calling window.
+        - accesses everything in the in that particular window.
+
+    links - https://www.w3schools.com/js/js_window.asp
+    https://developer.mozilla.org/en-US/docs/Web/API/Window
+*/
+
+
+/* ADD EVENT LISTENER - is a method that attaches a event handler to a HTML element.
+
+    syntax
+        - HTML element.addEventListener("event", function(){
+            useCapture
+        });
+
+            - 1st parameter is the type of event
+                - click
+                - onClick
+                - many many more
+            
+            2nd parameter is the function we want to call when the event occurs
+
+            3rd- is OPTIONAL the parameter is a boolean value deciding to use event bubbling or event capturing.
+            
+                Bubbling - inner most element is handled first
+                Capturing - the outer most element is handled first
+
+*/
+// -----------------------------------------------------------------------------
+
+/* adds an event listener to the window and logs the event.
+    event - keydown - when a key is pressed 
+        - console log the keycode.
+
+*/
+
+// window.addEventListener("keydown", function(event){
+//     console.log(event.keyCode);
+// });
+
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+
+/* assigns a variable to an element that has a data-key attribute
+- console logs the variable.
+*/
+
 window.addEventListener("keydown", function(event){
-    console.log(event);
+    const audio = document.querySelector(`audio[data-key = "${event.keyCode}"]`);
+    console.log(audio);
 });
+
+// -----------------------------------------------------------------------------
+
+
